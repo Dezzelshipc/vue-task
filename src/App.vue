ab08128e6949aa5fcc14cbb17cd03c69a115e6c8
Dezzelshipc <foler126@gmail.com>
2022-05-02 22:44:15 +1000

    Initial commit 2.0

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <HeaderButtons @newFile="file.text = $event" />
+  </header>
+  <TextArea :text="file.text" />
+</template>
+
+<script>
+import TextArea from './components/TextArea.vue'
+import HeaderButtons from './components/HeaderButtons.vue'
+
+// import fs from 'fs'
+// import pathModule from 'path'
+// import { computed, ref } from 'vue'
+
+export default {
+  components: {
+    TextArea,
+    HeaderButtons
+  },
+  data() {
+    return {
+      file: {
+        text: ""
+      }
+    }
+ }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TextArea.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="w-100 h-75 text-center p-3 position-relative">
+    <div class="w-100 h-100 border text-start p-1 border-secondary overflow-auto"
+      contenteditable="true"
+      v-text="text"
+      style="white-space: pre"
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+// import fs from 'fs'
+// import pathModule from 'path'
+// import { computed, ref } from 'vue'
+
+export default {
+  props: {
+    text: String
+  }
+}
+</script>
